008a21f292933a3e8f7a895a9ee7d833ca202963
cglaunchtech <103131392+cglaunchtech@users.noreply.github.com>
2024-03-10 22:47:38 -0400

    Create table in index.html to show results

--- db/queries.py
+++ db/queries.py
@@ -2,7 +2,7 @@ from db.db import execute_query, fetch_query
 
 
 def select_customers():
-    query = 'SELECT * FROM customers'
+    query = 'SELECT * FROM customers ORDER BY customer_id DESC LIMIT 10'
     customers = fetch_query(query)
     return customers
 
@@ -11,7 +11,7 @@ def search_customers(query):
     search_query = '%' + query + '%'
     query = '''SELECT * FROM customers 
                WHERE first_name LIKE ? OR last_name LIKE ? OR phone LIKE ? OR email LIKE ? 
-               OR street LIKE ? OR city LIKE ? OR state LIKE ? OR zipcode LIKE ?'''
+               OR street LIKE ? OR city LIKE ? OR state LIKE ? OR zip_code LIKE ?'''
     params = (
         search_query, search_query, search_query, search_query, search_query, search_query, search_query, search_query)
     customers = fetch_query(query, params)
@@ -19,12 +19,12 @@ def search_customers(query):
 
 
 def insert_customer(first_name, last_name, phone, email, street, city, state, zipcode):
-    query = 'INSERT INTO customers (first_name, last_name, phone, email, street, city, state, zipcode) VALUES (?, ?, ' \
+    query = 'INSERT INTO customers (first_name, last_name, phone, email, street, city, state, zip_code) VALUES (?, ?, ' \
             '?, ?, ?, ?, ?, ?) '
     execute_query(query, (first_name, last_name, phone, email, street, city, state, zipcode))
 
 
 def update_customer(first_name, last_name, phone, email, street, city, state, zipcode):
     query = 'UPDATE customers SET first_name = ?, last_name = ?, phone = ?, email = ?, street = ?, city = ?, ' \
-            'state = ?, zipcode = ? WHERE customer_id = ? '
+            'state = ?, zip_code = ? WHERE customer_id = ? '
     execute_query(query, (first_name, last_name, phone, email, street, city, state, zipcode))
