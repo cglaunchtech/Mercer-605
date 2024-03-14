2ac8f76bc75be9fb48f02330792817e1d6732a93
cglaunchtech <103131392+cglaunchtech@users.noreply.github.com>
2024-03-14 19:18:53 -0400

    Created modal and table

--- db/db.py
+++ db/db.py
@@ -25,3 +25,27 @@ def fetch_query(query, params=None):
     rows = cursor.fetchall()
     conn.close()
     return rows
+
+def select_past_orders(customer_id):
+    conn = sqlite3.connect('db/bike_store_db')
+    cursor = conn.cursor()
+    customer_id = 1445
+    query = '''
+        SELECT order_id, order_status, order_date, required_date, shipped_date,
+               product_id, product_name, model_year, product_list_price,
+               brand_name, category_name
+        FROM order_details
+        WHERE customer_id = ?
+    '''
+    cursor.execute(query, (customer_id,))
+    rows = cursor.fetchall()
+
+    # Print the fetched rows
+    for row in rows:
+        print(row)
+
+    # Close the connection
+    conn.close()
+    params = (customer_id,)
+    past_orders = fetch_query(query, params)
+    return past_orders
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
+from db.db import select_past_orders
 from db.queries import select_customers, search_customers
 
 app = Flask(__name__, template_folder='templates')
@@ -12,6 +14,12 @@ def index():
         customers = select_customers()  # Fetch all customers
     return render_template('index.html', customers=customers)
 
+@app.route('/get_past_orders', methods=['GET'])
+def get_past_orders():
+    customer_id = request.args.get('customer_id')
+    past_orders = select_past_orders(customer_id)
+    print("Past orders:", past_orders)  # Add this line to print past_orders
+    return jsonify(past_orders)
 
 if __name__ == '__main__':
     app.run()
